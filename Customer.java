1f4b9fcc2692c1bb4ad9611f61375211c982b3c9
Casey Scarborough <caseyscarborough@gmail.com>
2013-05-14 21:57:43 -0400

    Added delete functionality and error messages

--- Customer.java
+++ Customer.java
@@ -1,7 +1,11 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
@@ -24,7 +28,8 @@ import javax.swing.table.TableColumn;
 
 public class Customer {
 	
-	static JLabel lID, lFirstName, lLastName, lPhoneNumber, lEmailAddress, lCity, lState, lDateRegistered;
+	static JButton addCustomer, removeCustomer;
+	static JLabel errorMessage;
 	static JTextField tfFirstName, tfLastName, tfPhoneNumber, tfEmailAddress, tfCity, tfState, tfDateRegistered;
 	static java.util.Date dateDateRegistered, sqlDateRegistered;
 	
@@ -72,16 +77,71 @@ public class Customer {
 			System.out.println(e.getMessage()); 
 		}
 		
-		table.setFont(new Font("Sans-serif", Font.PLAIN, 18)); // Change font
+		table.setFont(new Font("Sans-serif", Font.PLAIN, 18));
 		table.setRowHeight(table.getRowHeight() + 8);
 		table.setAutoCreateRowSorter(true);
 		
 		JScrollPane scrollPane = new JScrollPane(table);
 		frame.add(scrollPane, BorderLayout.CENTER);
 		
-		JButton addCustomer = new JButton("Add Customer");
-		addCustomer.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		addCustomer = new JButton("Add Customer");
+		removeCustomer = new JButton("Remove Customer");
+		
+		ListenForAction actionListener = new ListenForAction();
+		addCustomer.addActionListener(actionListener);
+		removeCustomer.addActionListener(actionListener);
+		
+		tfFirstName = new JTextField("First Name", 6);
+		tfLastName = new JTextField("Last Name", 8);
+		tfPhoneNumber = new JTextField("Phone Number", 8);
+		tfEmailAddress = new JTextField("Email Address", 14);
+		tfCity = new JTextField("City", 8);
+		tfState = new JTextField("State", 3);
+		tfDateRegistered = new JTextField("Date Registered", 9);
+		
+		ListenForFocus focusListener = new ListenForFocus();
+		tfFirstName.addFocusListener(focusListener);
+		tfLastName.addFocusListener(focusListener);
+		tfPhoneNumber.addFocusListener(focusListener);
+		tfEmailAddress.addFocusListener(focusListener);
+		tfCity.addFocusListener(focusListener);
+		tfState.addFocusListener(focusListener);
+		tfDateRegistered.addFocusListener(focusListener);
+		
+		JPanel inputPanel = new JPanel();
+		inputPanel.add(tfFirstName);
+		inputPanel.add(tfLastName);
+		inputPanel.add(tfPhoneNumber);
+		inputPanel.add(tfEmailAddress);
+		inputPanel.add(tfCity);
+		inputPanel.add(tfState);
+		inputPanel.add(tfDateRegistered);
+		
+		inputPanel.add(addCustomer);
+		inputPanel.add(removeCustomer);
+		
+		errorMessage = new JLabel("");
+		errorMessage.setForeground(Color.red);
+		JPanel errorPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		errorPanel.add(errorMessage);
+		
+		
+		frame.add(inputPanel, BorderLayout.SOUTH);
+		frame.add(errorPanel, BorderLayout.NORTH);
+		
+		setColumnWidths(columns, 10, 60, 80, 100, 250, 80, 50, 100);
+		DefaultTableCellRenderer centerColumns = new DefaultTableCellRenderer();
+		centerColumns.setHorizontalAlignment(JLabel.CENTER);
+		TableColumn tc = table.getColumn("ID");
+		tc.setCellRenderer(centerColumns);
+		
+		frame.setSize(1110,500);
+		frame.setVisible(true);
+	}
+	
+	private static class ListenForAction implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == addCustomer) {
 				String firstName, lastName, phoneNumber, emailAddress, city, state, dateRegistered;
 				firstName = tfFirstName.getText();
 				lastName = tfLastName.getText();
@@ -97,8 +157,13 @@ public class Customer {
 					dateDateRegistered = dateFormatter.parse(dateRegistered);
 					sqlDateRegistered = new java.sql.Date(dateDateRegistered.getTime());
 				} catch (ParseException e1) {
-					System.out.println(e1.getMessage()); 
+					System.out.println(e1.getMessage());
+					if(e1.getMessage().toString().startsWith("Unparseable date:")) {
+						errorMessage.setText("The date should be in the following format: YYYY-MM-DD"); 
+					}
 				}
+				
+				int customerID = 0;
 				
 				try {
 					rows.moveToInsertRow();
@@ -112,52 +177,73 @@ public class Customer {
 					
 					rows.insertRow();
 					rows.updateRow();
-				} catch (SQLException e2) {
-					System.out.println(e2.getMessage()); 
-				}
-				
-				int customerID = 0;
-				try {
+					
 					rows.last();
 					customerID = rows.getInt(1);
-				} catch(SQLException e3) {
-					System.out.println(e3.getMessage()); 
+					Object[] customer = {customerID, firstName, lastName, phoneNumber, emailAddress, city, state, sqlDateRegistered};
+					defaultTableModel.addRow(customer);
+					errorMessage.setText("");
+				} catch (SQLException e2) {
+					System.out.println(e2.getMessage());
+					if (e2.getMessage().toString().startsWith("Data")) {
+						errorMessage.setText("A state should be a two-letter abbreviation."); 
+					}
 				}
-				
-				Object[] customer = {customerID, firstName, lastName, phoneNumber, emailAddress, city, state, sqlDateRegistered};
-				defaultTableModel.addRow(customer);
+			} else if (e.getSource() == removeCustomer) {
+				try {
+					defaultTableModel.removeRow(table.getSelectedRow());
+					rows.absolute(table.getSelectedRow());
+					rows.deleteRow();
+				} catch(SQLException e1) {
+					System.out.println(e1.getMessage());
+					errorMessage.setText(e1.getMessage());
+				} catch(ArrayIndexOutOfBoundsException e1) {
+					System.out.println(e1.getMessage());
+					errorMessage.setText("To delete a customer, you must first select a row.");
+				}
 			}
-		});
+		}
+	}
+	
+	// My terrible and possibly hack-ish way of implementing 'placeholders' in the JTextFields.
+	private static class ListenForFocus implements FocusListener {
 		
-		tfFirstName = new JTextField("First Name", 8);
-		tfLastName = new JTextField("Last Name", 12);
-		tfPhoneNumber = new JTextField("Phone Number", 8);
-		tfEmailAddress = new JTextField("Email Address", 20);
-		tfCity = new JTextField("City", 8);
-		tfState = new JTextField("State", 3);
-		tfDateRegistered = new JTextField("Date Registered", 10);
+		public void focusGained(FocusEvent e) {
+			if(tfFirstName.getText().equals("First Name") && e.getSource() == tfFirstName) {
+				tfFirstName.setText("");
+			} else if(tfLastName.getText().equals("Last Name") && e.getSource() == tfLastName) {
+				tfLastName.setText("");
+			} else if(tfPhoneNumber.getText().equals("Phone Number") && e.getSource() == tfPhoneNumber) {
+				tfPhoneNumber.setText("");
+			} else if(tfEmailAddress.getText().equals("Email Address") && e.getSource() == tfEmailAddress) {
+				tfEmailAddress.setText("");
+			} else if(tfCity.getText().equals("City") && e.getSource() == tfCity) {
+				tfCity.setText("");
+			} else if(tfState.getText().equals("State") && e.getSource() == tfState) {
+				tfState.setText("");
+			} else if(tfDateRegistered.getText().equals("Date Registered") && e.getSource() == tfDateRegistered) {
+				tfDateRegistered.setText("");
+			}
+		}
+
+		public void focusLost(FocusEvent e) {
+			if(tfFirstName.getText().equals("") && e.getSource() == tfFirstName) {
+				tfFirstName.setText("First Name");
+			} else if(tfLastName.getText().equals("") && e.getSource() == tfLastName) {
+				tfLastName.setText("Last Name");
+			} else if(tfPhoneNumber.getText().equals("") && e.getSource() == tfPhoneNumber) {
+				tfPhoneNumber.setText("Phone Number");
+			} else if(tfEmailAddress.getText().equals("") && e.getSource() == tfEmailAddress) {
+				tfEmailAddress.setText("Email Address");
+			} else if(tfCity.getText().equals("") && e.getSource() == tfCity) {
+				tfCity.setText("City");
+			} else if(tfState.getText().equals("") && e.getSource() == tfState) {
+				tfState.setText("State");
+			} else if(tfDateRegistered.getText().equals("") && e.getSource() == tfDateRegistered) {
+				tfDateRegistered.setText("Date Registered");
+			}
+		}
 		
-		JPanel inputPanel = new JPanel();
-		inputPanel.add(tfFirstName);
-		inputPanel.add(tfLastName);
-		inputPanel.add(tfPhoneNumber);
-		inputPanel.add(tfEmailAddress);
-		inputPanel.add(tfCity);
-		inputPanel.add(tfState);
-		inputPanel.add(tfDateRegistered);
-		
-		inputPanel.add(addCustomer);
-		
-		frame.add(inputPanel, BorderLayout.SOUTH);
-		
-		setColumnWidths(columns, 10, 60, 80, 100, 250, 80, 50, 100);
-		DefaultTableCellRenderer centerColumns = new DefaultTableCellRenderer();
-		centerColumns.setHorizontalAlignment(JLabel.CENTER);
-		TableColumn tc = table.getColumn("ID");
-		tc.setCellRenderer(centerColumns);
-		
-		frame.setSize(1200,500);
-		frame.setVisible(true);
 	}
 	
 	public static void setColumnWidths(Object[] columns, int...widths) {
